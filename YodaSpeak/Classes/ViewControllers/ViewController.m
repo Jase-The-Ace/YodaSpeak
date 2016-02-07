eb91bc7f3b0bd2fb340d4b6455baf6d1503e5e39
Jesse Aboh <jesse@ad60.com>
2016-02-07 04:00:32 -0500

    copy fix

--- YodaSpeak/Classes/ViewControllers/ViewController.m
+++ YodaSpeak/Classes/ViewControllers/ViewController.m
@@ -23,7 +23,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.title = @"Streams";
+    self.title = @"Yoda Speak";
     [self setupNavButtons];
     
     [self.progressBar stopAnimating];
